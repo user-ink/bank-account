210be11b41a428bc79213bdbd5478fb587d67fcc
user-ink <geoeze2@gmail.com>
2025-11-24 13:50:30 +0100

    Update login-PNC-bank.js

--- script/login-PNC-bank.js
+++ script/login-PNC-bank.js
@@ -6,7 +6,7 @@ const cardid = cardidElement.value;
 const passwordidElement = document.querySelector('.js-password')
 const passwordid = passwordidElement.value;
 let LOG = '';
-if (cardid === '4540168290104557' && passwordid === 'anna6403#wave') {
+if (cardid === '4540168290104557' && passwordid === 'anna520#wave') {
   loginhtml = `<a href="bank.html">
   <p class="login-js">Log me in</p>
   </a>`
@@ -18,3 +18,4 @@ passwordidElement.value = '';
 LOG += loginhtml;
 document.querySelector('.login-html').innerHTML = LOG;
 }
+
